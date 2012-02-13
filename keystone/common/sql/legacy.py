d704bcfda937d9dc3ccdd65a1d7a91f3da4dd069
termie <github@anarkystic.com>
2012-02-13 15:36:07 -0800

    wip
    Change-Id: I96bebed358781f3eb18a73f19add9667363d9716

--- keystone/common/sql/legacy.py
+++ keystone/common/sql/legacy.py
@@ -55,6 +55,7 @@ class LegacyMigration(object):
         self._migrate_tenants()
         self._migrate_users()
         self._migrate_roles()
+        self._migrate_user_roles()
         self._migrate_tokens()
 
     def dump_catalog(self, path):
@@ -66,9 +67,62 @@ class LegacyMigration(object):
 
     def _migrate_tenants(self):
         for x in self._data['tenants']:
+            # map
             new_dict = {'description': x.get('desc', ''),
                         'id': x.get('uid', x.get('id')),
                         'enabled': x.get('enabled', True)}
             new_dict['name'] = x.get('name', new_dict.get('id'))
+            # track internal ids
+            self._tenant_map[new_dict['id']] = x.get('id')
+            # create
             self.identity_driver.create_tenant(new_dict['id'], new_dict)
 
+    def _migrate_users(self):
+        for x in self._data['users']:
+            # map
+            new_dict = {'email': x.get('email', ''),
+                        'password': x.get('password', None),
+                        'id': x.get('uid', x.get('id')),
+                        'enabled': x.get('enabled', True)}
+            if x.get('tenant_id'):
+                new_dict['tenant_id'] = self._tenant_map.get(x['tenant_id'])
+            new_dict['name'] = x.get('name', new_dict.get('id'))
+            # track internal ids
+            self._user_map[new_dict['id']] = x.get('id')
+            # create
+            self.identity_driver.create_user(new_dict['id'], new_dict)
+            if new_dict.get('tenant_id'):
+                self.identity_driver.add_user_to_tenant(new_dict['tenant_id'],
+                                                        new_dict['id'])
+
+    def _migrate_roles(self):
+        for x in self._data['roles']:
+            # map
+            new_dict = {'id': x['id'],
+                        'name': x.get('name', x['id'])}
+            # track internal ids
+            self._role_map[new_dict['id']] = x.get('id')
+            # create
+            self.identity_driver.create_role(new_dict['id'], new_dict)
+
+    def _migrate_user_roles(self):
+        for x in self._data['user_roles']:
+            # map
+            if (not x.get('user_id')
+                or not x.get('tenant_id')
+                or not x.get('role_id')):
+                continue
+            user_id = self._user_map[x['user_id']]
+            tenant_id = self._tenant_map[x['tenant_id']]
+            role_id = self._role_map[x['role_id']]
+
+            try:
+                self.identity_driver.add_user_to_tenant(tenant_id, user_id)
+            except Exception:
+                pass
+
+            self.identity_driver.add_role_to_user_and_tenant(
+                    user_id, tenant_id, role_id)
+
+    def _migrate_tokens(self):
+        pass
