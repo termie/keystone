ea4fa932c586de8a2fa8a7b6d0b8530a18268422
termie <github@anarkystic.com>
2012-02-27 05:54:14 -0800

    revert keystone/service.py
    Change-Id: If7ef2a846617fae4b38b45058e060bce5f3d31cf

--- keystone/service.py
+++ keystone/service.py
@@ -217,11 +217,8 @@ class TokenController(wsgi.Application):
             if username:
                 user_ref = self.identity_api.get_user_by_name(
                         context=context, user_name=username)
-                if (not user_ref):
-                    raise exception.Forbidden()
-                user_id = user_ref['id']
-            else:
-                user_id = auth['passwordCredentials'].get('userId', None)
+                if user_ref:
+                    user_id = user_ref['id']
 
             # more compat
             tenant_id = auth.get('tenantId', None)
@@ -323,10 +320,7 @@ class TokenController(wsgi.Application):
 
         """
         # TODO(termie): this stuff should probably be moved to middleware
-        try:
-            self.assert_admin(context)
-        except:
-            raise exception.Unauthorized()
+        self.assert_admin(context)
 
         token_ref = self.token_api.get_token(context=context,
                                              token_id=token_id)
